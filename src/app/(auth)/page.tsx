7c4e107818f3c9f8a33b7c6b12bc0a5fa2606d06
Ekaterina Vinnikova <red-white.katya@mail.ru>
2025-11-23 11:00:40 -0500

    fix: delete page from auth

--- src/app/(auth)/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from '~/shared/lib/navigation';
-
-export default function RootPage({ params }: { params: { locale: string } }) {
-  redirect({
-    href: '/sign-in',
-    locale: params.locale,
-  });
-}
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,8 @@
-import { useTranslations } from 'next-intl';
+import { redirect } from '~/shared/lib/navigation';
 
-export default function HomePage() {
-  const t = useTranslations('translation');
-
-  return <div>{t('HomePage.title')}</div>;
+export default function RootPage({ params }: { params: { locale: string } }) {
+  redirect({
+    href: '/sign-in',
+    locale: params.locale,
+  });
 }
